21b544069798e131df9eb524f623289c72af00e0
vusal965 <vusalbaxsiyev756@gmail.com>
2024-06-21 16:44:19 +0400

    Home work

---
+++ app.js
@@ -0,0 +1,14 @@
+
+let fullName = prompt("adinizi ve soyadinizi daxil edin")
+console.log("salam " + fullName.split(" ")[0]);
+
+
+let number1 = prompt("4 reqemli eded daxil edin");
+let number2 = number1.split("");
+let result = (+number2[0] + +number2[1] + +number2[2] + +number2[3]);
+console.log(result);
+
+
+
+
+
